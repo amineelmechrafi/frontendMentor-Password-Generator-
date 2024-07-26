d35a2aa081eeb73508117776d7b80fcda711a98d
amineelmechrafi <aminemechrafiaminne@gmail.com>
2024-07-26 15:51:20 +0000

    "adding the ui for the password generator app"

---
+++ src/components/Strenght.tsx
@@ -0,0 +1,16 @@
+
+const Strenght = () => {
+  return (
+    <div className='flex justify-between items-center gap-2'>
+      <h2>Level</h2>
+      <div className='flex justify-between items-center gap-1'>
+            <div className="rect"></div>
+            <div className="rect"></div>
+            <div className="rect"></div>
+            <div className="rect"></div>
+      </div>
+    </div>
+  )
+}
+
+export default Strenght
